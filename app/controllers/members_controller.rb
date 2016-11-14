065f721d50b8205b370ba738c22d634ad9a05daa
rhamill1 <rhamill11@gmail.com>
2016-11-14 10:13:49 -0800

    sets up rails app

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,23 @@
+class MembersController < ApplicationController
+
+  def index
+    @members = Member.all
+  end
+
+  def new
+    @member = Member.new
+  end
+
+  def create
+    @member = Member.create(member_params)
+    redirect_to root_path
+  end
+
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :avatar)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+
+  root "members#index"
+
+  resources :members
+end
